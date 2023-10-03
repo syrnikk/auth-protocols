b2e4e0854d24705064a0b0acd9d22fc89795f9c1
Kacper Syrnik <kacper.syrnik1@gmail.com>
2023-10-03 17:35:07 +0200

    Create backend module

---
+++ auth-protocols-backend/src/main/java/com/syrnik/authprotocolsbackend/AuthProtocolsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.syrnik.authprotocolsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthProtocolsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthProtocolsBackendApplication.class, args);
+    }
+
+}
---
+++ auth-protocols-backend/src/test/java/com/syrnik/authprotocolsbackend/AuthProtocolsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syrnik.authprotocolsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthProtocolsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
