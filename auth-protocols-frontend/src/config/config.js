863d58fffe1963c6d5bb604c8f6232aed22097c2
Kacper Syrnik <kacper.syrnik1@gmail.com>
2023-12-03 15:43:08 +0100

    Add env variables for frontend

--- auth-protocols-frontend/src/config/config.js
+++ auth-protocols-frontend/src/config/config.js
@@ -1,9 +1,9 @@
 const config = {
-  API_URL: "http://localhost:5173",
-  OIDC_AUTHORITY: "http://localhost:8080/realms/auth-protocols-oidc",
-  OIDC_CLIENT_ID: "auth-protocols-client-oidc",
-  OIDC_REDIRECT_URI: "http://localhost:5173",
-  OIDC_POST_LOGOUT_REDIRECT_URI: "http://localhost:5173",
+  API_URL: import.meta.env.VITE_API_URL,
+  OIDC_AUTHORITY: import.meta.env.VITE_OIDC_AUTHORITY,
+  OIDC_CLIENT_ID: import.meta.env.VITE_OIDC_CLIENT_ID,
+  OIDC_REDIRECT_URI: import.meta.env.VITE_OIDC_REDIRECT_URI,
+  OIDC_POST_LOGOUT_REDIRECT_URI: import.meta.env.VITE_OIDC_POST_LOGOUT_REDIRECT_URI,
 };
 
 export default config;
