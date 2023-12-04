8a26b3ab1205b7e9759d324e22fd780ce9ed1e2a
Kacper Syrnik <kacper.syrnik1@gmail.com>
2023-12-04 22:10:18 +0100

    Add SPN to env variable

--- kerberos/init/init-script.sh
+++ kerberos/init/init-script.sh
@@ -5,8 +5,8 @@ FLAG_FILE="/etc/krb5/init/.initialized"
 if [ ! -f "$FLAG_FILE" ]; then
     # Initialize principals and keytabs
     kadmin.local -q "addprinc -pw password johndoe-kerberos@AUTH-PROTOCOLS.COM"
-    kadmin.local -q "addprinc -randkey HTTP/desktop-a194p2n@AUTH-PROTOCOLS.COM"
-    kadmin.local -q "ktadd -k /etc/krb5/keytabs/krb5.keytab HTTP/desktop-a194p2n@AUTH-PROTOCOLS.COM"
+    kadmin.local -q "addprinc -randkey ${KERBEROS_SPN}"
+    kadmin.local -q "ktadd -k /etc/krb5/keytabs/krb5.keytab ${KERBEROS_SPN}"
 
     # Create a flag file to indicate completion
     touch "$FLAG_FILE"
